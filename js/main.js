4dc3cbb1ab1e0b15e739cfe014af59a2642edabb
Sonika Shetty <shettysonika@gmail.com>
2021-10-10 15:57:22 +0530

    second home commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let menuIcon = document.getElementById("menu-icon");
 let bar=document.querySelectorAll(".menu-icon .bar");
-let menuMobile=document.querySelector(".nav-bar2 .mobile");
+let menuMobile=document.querySelector(".nav-bar2 .mobile .links");
 menuIcon.addEventListener("click",()=>{
     menuIcon.classList.toggle("anim-moveright");
     bar[0].classList.toggle("anim-rot-45c");
