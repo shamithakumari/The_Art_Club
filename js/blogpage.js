583a71b76316ac2141bb445af53b7ecee5ad0f1f
shamitha <shamithakumarims@gmail.com>
2021-11-20 15:22:58 +0530

    adds basic blog upload form to the blog page

---
+++ js/blogpage.js
@@ -0,0 +1,9 @@
+    let addart=document.querySelector('.addart');
+    let addartform=document.querySelector('.addart-form');
+    let addartclose=document.querySelector('.addart-form .close');
+    addart.addEventListener('click', ()=>{
+        addartform.classList.add("addart-form-show");
+    })
+    addartclose.addEventListener('click', ()=>{
+        addartform.classList.remove("addart-form-show");
+    })
