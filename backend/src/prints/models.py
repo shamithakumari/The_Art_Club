dabf559679e2b9f4197aff4d5c5a09763ee87dbc
shamitha <shamithakumarims@gmail.com>
2021-12-16 02:51:40 +0530

    [Shammi] fixes print screen data issue

--- backend/src/prints/models.py
+++ backend/src/prints/models.py
@@ -9,7 +9,7 @@ class Print(models.Model):
     description=models.TextField(blank=False)
     cost=models.DecimalField(max_digits=7,decimal_places=2,blank=False)
     time_created=models.DateTimeField(auto_now_add=True,blank=False)
-    print_image=models.ImageField(upload_to='prints/images/',blank=False)
+    print_image=models.ImageField(upload_to='prints/images',blank=False)
 
     def __str__(self):
         return self.title+" ( By:"+self.artist.first_name+")"+" -Rs."+str(self.cost)
